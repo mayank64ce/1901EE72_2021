98c25b8b5118c32aa36aaf24bfe043eb947dbb09
mayank64ce <mayank_1901ee72@iitp.ac.in>
2021-08-16 20:13:31 +0530

    Completed tut01

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,1 +1,25 @@
-print('My name is Mayank. My Roll number is 1901EE72.')
+def meraki_helper(n: str):
+    l = len(n)
+    for i in range(l-1):
+        if abs(ord(n[i])-ord(n[i+1])) != 1:
+            print("No", n, "is not a meraki number.")
+            return False
+    print("Yes", n, "is a meraki number.")
+    return True
+
+
+def meraki(a: list):
+    meraki_count = 0
+    non_meraki_count = 0
+    for x in a:
+        if meraki_helper(x):
+            meraki_count += 1
+        else:
+            non_meraki_count += 1
+
+    print("The input list contains {} meraki and {} non meraki numbers.".format(
+        meraki_count, non_meraki_count))
+
+
+a = list(input().split(' '))
+meraki(a)
