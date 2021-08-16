4fb9db910816b10be13c533c1acc02cbb50d8ff0
mayank64ce <mayank_1901ee72@iitp.ac.in>
2021-08-16 20:23:05 +0530

    Updated tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,7 +1,22 @@
+"""
+    author: mayank64ce
+    This program tells wether positive number is meraki number or not.
+"""
+
+
 def meraki_helper(n: str):
+    """This method determines wether the number 'n' (in string format) is 
+    meraki number or not by comparing ASCII values of adjacent digits.
+
+    Args:
+        n (str): [input number in string format]
+
+    Returns:
+        [boolean]: [returns True iff n is meraki]
+    """
     l = len(n)
     for i in range(l-1):
-        if abs(ord(n[i])-ord(n[i+1])) != 1:
+        if abs(ord(n[i])-ord(n[i+1])) != 1:  # ord(c) returns the ASCII value of charachter c
             print("No", n, "is not a meraki number.")
             return False
     print("Yes", n, "is a meraki number.")
@@ -9,8 +24,14 @@ def meraki_helper(n: str):
 
 
 def meraki(a: list):
-    meraki_count = 0
-    non_meraki_count = 0
+    """This method counts the number of meraki and non-meraki numbers in the
+        given list.
+
+    Args:
+        a (list): [a list of (positive) numbers in string format]
+    """
+    meraki_count = 0        # variable for storing count of meraki numbers
+    non_meraki_count = 0    # variable for storing count of non-meraki numbers
     for x in a:
         if meraki_helper(x):
             meraki_count += 1
@@ -20,6 +41,8 @@ def meraki(a: list):
     print("The input list contains {} meraki and {} non meraki numbers.".format(
         meraki_count, non_meraki_count))
 
+
+# Driver function
 
 a = list(input().split(' '))
 meraki(a)
